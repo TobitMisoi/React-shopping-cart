01e8643cebb199c1480359a87045e261d522f903
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-04-26 12:29:42 +0300

    add eslint module0resolver

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 // Feature 1
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+import { Component } from "react";
+import data from "./data.json";
 
-      </header>
-      <main>
 
-      </main>
-      <footer>
-        footer
-      </footer>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      products: data.products,
+      size: '',
+      sort: ''
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <a href="/">
+            React Shopping Cart
+        </a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              Product
+            </div>
+            <div className="sidebar">
+              Cart items
+            </div>
+          </div>
+        </main>
+        <footer>
+          All Rights Reserved
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
