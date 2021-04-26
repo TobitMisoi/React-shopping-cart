0e80d2cabaf43f1cfd5ce5722548fa643de5ea4c
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-04-26 20:30:42 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
+import React from 'react'
+import { Footer } from './components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-      <main>
-
-      </main>
-      <footer>
-        footer
-      </footer>
+    <div className='App'>
+      <header className='App-header'></header>
+      <main></main>
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
